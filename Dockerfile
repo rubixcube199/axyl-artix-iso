cbd3e7d61ad07beb1bfa1707da63c6ec2d4acb48
Angelo Fallaria <ba.fallaria@gmail.com>
2022-04-09 13:29:27 +0800

    build: fix errors in Docker build

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,10 @@ RUN pacman-key --init
 RUN pacman-key --populate
 
 WORKDIR /axyl-artix-iso
-COPY ./artixiso/ ./artixiso/
+COPY . .
 
-CMD cd ./artixiso/artools-workspace/iso-profiles/ ; ./build.sh
+RUN ln -sf "$(pwd)/artixiso/artools-workspace" ~/
+RUN mkdir ~/.config
+RUN ln -sf "$(pwd)/config/artools" ~/.config/
+
+CMD cd /root/artools-workspace/iso-profiles ; ./build.sh
--- docker-build.sh
+++ docker-build.sh
@@ -6,4 +6,4 @@ if [ "$?" -ne 0 ]; then
 fi
 
 mkdir build pacman-cache
-sudo docker run --privileged --mount type=bind,source="$(pwd)"/build,target=/root/artools-workspace --mount type=bind,source="$(pwd)"/pacman-cache,target=/var/cache/pacman/pkg -t axyl-artix-build
+sudo docker run --privileged --mount type=bind,source="$(pwd)"/build,target=/root/artools-workspace/iso --mount type=bind,source="$(pwd)"/pacman-cache,target=/var/cache/pacman/pkg -t axyl-artix-build
