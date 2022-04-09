5d03cc3e7c5eeba4041a0cebc9df3b85dc142d29
Kungger-git <matthewriel63@gmail.com>
2022-04-09 18:43:55 +0800

    feat: require to run script as `sudo`

--- docker-build.sh
+++ docker-build.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
-sudo docker build -t axyl-artix-build ./
+
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
+docker build -t axyl-artix-build ./
 
 if [ "$?" -ne 0 ]; then
     exit 1
 fi
 
 mkdir build pacman-cache
-sudo docker run --privileged --mount type=bind,source="$(pwd)"/build,target=/root/artools-workspace/iso --mount type=bind,source="$(pwd)"/pacman-cache,target=/var/cache/pacman/pkg -t axyl-artix-build &&\
+docker run --privileged --mount type=bind,source="$(pwd)"/build,target=/root/artools-workspace/iso --mount type=bind,source="$(pwd)"/pacman-cache,target=/var/cache/pacman/pkg -t axyl-artix-build &&\
 echo "Axyl ISO build successful! The ISO was placed in ./build."
