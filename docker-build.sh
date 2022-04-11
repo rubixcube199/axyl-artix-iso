67aa88edae461274bc92b564da74e02cdcf2a53e
Adarsh <83777943+theCode-Breaker@users.noreply.github.com>
2022-04-11 17:19:46 +0530

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -12,7 +12,6 @@ if [ "$?" -ne 0 ]; then
     exit 1
 fi
 
-modprobe loop
 mkdir build pacman-cache
 docker run --privileged --mount type=bind,source="$(pwd)"/build,target=/root/artools-workspace/iso --mount type=bind,source="$(pwd)"/pacman-cache,target=/var/cache/pacman/pkg -t axyl-artix-build &&\
 echo "Axyl ISO build successful! The ISO was placed in ./build."
