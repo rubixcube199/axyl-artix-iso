8c010ec54dc0ebd3d9a05838992618e4fd56784d
Kungger-git <matthewriel63@gmail.com>
2022-04-13 11:31:54 +0800

    feat: Renamed variables.

--- docker-build.sh
+++ docker-build.sh
@@ -10,25 +10,30 @@ fi
 _user=`echo ${SUDO_USER:-$(whoami)}`
 _gid=`echo ${SUDO_GID}`
 _group=`cat /etc/group | grep ${_gid} | cut -d: -f1 | head -1`
-_path="$(pwd)/build/"
-_iso="$(pwd)/Axyl-Iso/"
+
+## iso Paths and file
+_build="$(pwd)/build/"
+_output="$(pwd)/Axyl-Iso/"
 
 ## iso filename format
-_iso_name="axyl-runit"
-_iso_version="$(date +%Y.%m.%d)"
+_name="axyl-runit"
+_version="$(date +%Y.%m.%d)"
 _arch="x86_64"
+_iso_filename="${_name}-${_version}-${_arch}.iso"
 
 _finalize_build() {
+  local _file=$(find ${_build} -type f -name "*iso")
+
   ## Change iso filename to current format
   echo "+---------------------->>"
-  echo "[*] Move Iso ${_iso} as ${_iso_name}-${_iso_version}-${_arch}.iso..."
-  mkdir -p ${_iso}
-  find ${_path} -type f -name "*.iso" -exec mv {} ${_iso}${_iso_name}-${_iso_version}-${_arch}.iso \;
+  echo "[*] Move Iso '$(basename ${_file})' as '${_iso_filename}'..."
+  mkdir -p ${_output}
+  find ${_build} -type f -name "*.iso" -exec mv {} ${_output}${_iso_filename} \;
 
   ## Change ownership
   echo "+---------------------->>"
-  echo "[*] Change ${_iso} ownership to '${_user}'..."
-  chown -R ${_user}:${_group} ${_iso}
+  echo "[*] Change ${_output} ownership to '${_user}'..."
+  chown -R ${_user}:${_group} ${_output}
 
   ## Remove empty build directory
   rm -rf ./build
@@ -36,9 +41,9 @@ _finalize_build() {
 
 ## Delete existing Axyl-Iso directory from home user
 echo "+---------------------->>"
-echo "[*] Delete existing ${_iso}..."
-if [[ -d ${_iso} ]]; then
-  rm -rf ${_iso}
+echo "[*] Delete existing ${_output}..."
+if [[ -d ${_output} ]]; then
+  rm -rf ${_output}
 fi
 
 ## Build Docker Image
@@ -49,7 +54,7 @@ if [ "$?" -ne 0 ]; then
 fi
 
 ## Run The Docker Image
-mkdir -p ${_path} ./pacman-cache
+mkdir -p ${_build} ./pacman-cache
 docker run --privileged \
            --mount type=bind,source="$(pwd)"/build,target=/root/artools-workspace/iso \
            --mount type=bind,source="$(pwd)"/pacman-cache,target=/var/cache/pacman/pkg \
