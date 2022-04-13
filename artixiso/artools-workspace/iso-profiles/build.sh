557c6b07f2393c1505be6b2df21383168022cbd5
Angelo Fallaria <ba.fallaria@gmail.com>
2022-04-13 19:29:52 +0800

    build: remove commands related to AUR packages

--- artixiso/artools-workspace/iso-profiles/build.sh
+++ artixiso/artools-workspace/iso-profiles/build.sh
@@ -11,32 +11,6 @@ buildiso -i $init -p base -x
 
 artix-chroot /var/lib/artools/buildiso/base/artix/rootfs bash -c "pacman-key --init; pacman-key --populate artix;pacman-key --populate archlinux; pacman -Syy"
 
-printf "\033[1;33mAUR_BUILD:\033[0m Reading package list.\n"
-for package in $(cat ./AUR_PACKAGES | grep -vE "^#.*$")
-do
-  printf "\033[1;33mAUR_BUILD:\033[0m Cloning ${package}.\n"
-  artix-chroot /var/lib/artools/buildiso/base/artix/rootfs \
-	  git clone https://aur.archlinux.org/${package}.git /etc/aur_building/$package
-done
-
-printf "\033[1;33mAUR_BUILD:\033[0m Building packages.\n"
-
-artix-chroot /var/lib/artools/buildiso/base/artix/rootfs bash -c "chmod 777 -R /etc/aur_building"
-dirs=$(artix-chroot /var/lib/artools/buildiso/base/artix/rootfs find /etc/aur_building -mindepth 1 -maxdepth 1 -type d)
-for dir in $dirs
-do
-	artix-chroot /var/lib/artools/buildiso/base/artix/rootfs bash -c "cd $dir; sudo -u $non_root_user makepkg -sfci --noconfirm --needed"
-done
-
-if [[ $(artix-chroot /var/lib/artools/buildiso/base/artix/rootfs pacman -Qtdq) != "" ]];then
-    artix-chroot /var/lib/artools/buildiso/base/artix/rootfs \
-		yes | sudo pacman -Rns $(pacman -Qtdq)
-    printf "\u001b[31mPACMAN:\033[0m Orphans found and removed, exiting script.\n"
-else
-    printf "\u001b[31mPACMAN:\033[0m No orphans found, exiting script.\n"
-fi
-rm -rf /etc/aur_building
-
 buildiso -i $init -p base -sc
 buildiso -i $init -p base -bc
 buildiso -i $init -p base -zc || exit 1
