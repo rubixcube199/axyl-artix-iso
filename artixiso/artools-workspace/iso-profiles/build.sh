bc4d33156f3f6dbb37ae162a02c086c18eeb3122
Kungger-git <matthewriel63@gmail.com>
2022-04-19 12:15:06 +0800

    feat: add checking of `AUR_PACKAGES` txt file & checking if file has contents of package names.

--- artixiso/artools-workspace/iso-profiles/build.sh
+++ artixiso/artools-workspace/iso-profiles/build.sh
@@ -11,7 +11,7 @@ init=runit
 chroot_dir="/var/lib/artools/buildiso/base/artix/rootfs"
 
 ## Set color characters
-set_color() {
+_set_color() {
     if [ -t 1 ]; then
         RED=$(printf '\033[31m')
         GREEN=$(printf '\033[32m')
@@ -31,25 +31,76 @@ set_color() {
 
 ## Show an INFO message
 _msg_info() {
-    local _msg="${1}"
-    printf '%s[ARTIX-CHROOT] INFO:%s %s\n' "${YELLOW}${BOLD}" "${RESET}" "${BOLD}${_msg}${RESET}"
+    local _subject="${1}"
+    local _msg="${2}"
+    printf '%s[%s] INFO: %s\n' "${YELLOW}${BOLD}" "${_subject}" "${RESET}${BOLD}${_msg}${RESET}"
 }
 
 ## Customize installation.
 _make_customize_chroot() {
-    _msg_info "Running customize_chroot.sh in '${chroot_dir}' chroot..."
+    _msg_info "ARTIX-CHROOT" "Running customize_chroot.sh in '${chroot_dir}' chroot..."
     chmod +x "${chroot_dir}/root/customize_chroot.sh"
     artix-chroot "${chroot_dir}" "/root/customize_chroot.sh"
     rm -f "${chroot_dir}/root/customize_chroot.sh"
-    _msg_info "Done! customize_chroot.sh run successfully..."
+    _msg_info "ARTIX-CHROOT" "Done! customize_chroot.sh run successfully..."
 }
 
-set_color
+## Build Packages from `AUR_PACKAGES` list.
+_build_aur_packages() {
+    local _packages="$1"
+
+    for package in ${_packages}
+    do
+      _msg_info "AUR_BUILD" "Cloning ${package}."
+      artix-chroot ${chroot_dir} \
+	      git clone https://aur.archlinux.org/${package}.git /etc/aur_building/$package
+    done
+
+    _msg_info "AUR_BUILD" "Building packages"
+    artix-chroot ${chroot_dir} bash -c "chmod 777 -R /etc/aur_building"
+    dirs=$(artix-chroot ${chroot_dir} find /etc/aur_building -mindepth 1 -maxdepth 1 -type d)
+    for dir in $dirs
+    do
+            artix-chroot ${chroot_dir} bash -c "cd $dir; sudo -u $non_root_user makepkg -sfci --noconfirm --needed"
+    done
+
+    if [[ $(artix-chroot ${chroot_dir} pacman -Qtdq) != "" ]];then
+        artix-chroot ${chroot_dir} \
+		    yes | sudo pacman -Rns $(pacman -Qtdq)
+        printf "\u001b[31mPACMAN:\033[0m Orphans found and removed, exiting script.\n"
+        _msg_info "PACMAN" "Orphans found and removed, exiting script."
+    else
+        _msg_info "PACMAN" "No orphans found, exiting script."
+    fi
+    rm -rf /etc/aur_building
+}
+
+## Read list of package names from `AUR_PACKAGES` file.
+_read_aur_packages() {
+  _msg_info "AUR_BUILD" "Reading package list."
+  local _list=$(cat ./AUR_PACKAGES | grep -vE "^#.*$")
+
+  if [[ ! ${_list} == "" ]]; then
+      _build_aur_packages ${_list}
+  else
+      _msg_info "WARNING" "No Package names were found in the 'AUR_PACKAGES' file. Skipping..."
+  fi
+}
+
+
+## main
+_set_color
+trap "" EXIT
 buildiso -i $init -p base -x
 
 ## Update System
-_msg_info "Live environment pacman system update & populate keyrings..."
+_msg_info "ARTIX-CHROOT" "Live environment pacman system update & populate keyrings..."
 artix-chroot ${chroot_dir} bash -c "pacman-key --init; pacman-key --populate artix; pacman-key --populate archlinux; pacman -Syy"
+
+## Check if `AUR_PACKAGES` txt file exists.
+if [[ -e $(pwd)/AUR_PACKAGES ]]; then
+    _read_aur_packages
+fi
 
 ## Check if `customize_chroot.sh` exists.
 if [[ -e $(pwd)/base/root-overlay/root/customize_chroot.sh ]]; then
